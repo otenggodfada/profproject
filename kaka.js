2ea972aee686e17ff66ca96626d681d090c9f1c3
Oteng Emmanuel <{ID}+{username}@users.noreply.github.com>
2025-03-08 15:27:38 +0000

    ddddddd

---
+++ kaka.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let element = document.getElementById("lolo");
+    if (element) {
+        element.textContent = 'ddddd222';
+    } else {
+        console.log("Element with ID 'lolo' not found!");
+    }
+});

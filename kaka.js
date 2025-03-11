3643b986e382ac8c2d485787acd161f5d519e8e9
Oteng Emmanuel <{ID}+{username}@users.noreply.github.com>
2025-03-11 01:22:59 +0000

    dddddddd

--- kaka.js
+++ kaka.js
@@ -1,6 +1,6 @@
 function createBottomNav() {
     const nav = document.createElement("nav");
-    nav.className = "fixed bottom-0 left-0 right-0 bg-[#172554] shadow-lg border-t border-gray-300 md:hidden";
+    nav.className = "fixed bottom-0 z-50 left-0 right-0 bg-[#172554] shadow-lg border-t border-gray-300 md:hidden";
     
     const navContainer = document.createElement("div");
     navContainer.className = "flex justify-around items-center py-3";

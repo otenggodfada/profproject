2d916a1ca0c311ad698193d6bd7243967f12b8ac
Oteng Emmanuel <{ID}+{username}@users.noreply.github.com>
2025-03-18 04:57:32 +0000

    dddddd

--- mycoursesscrpt.js
+++ mycoursesscrpt.js
@@ -292,8 +292,8 @@ function renderCourses(courses) {
             <summary onclick="turnonfixed()" class="font-semibold w-full h-full text-center cursor-pointer list-none">
               <i class="fas fa-book-open"></i> <span>Continue Learning</span>
             </summary>
-            <div id="modal-container" style="display:block;" class="z-50 top-0 bottom-0 right-0 left-0  w-screen h-full bg-black bg-opacity-80 flex items-center justify-center mb-28 ">
-              <div class="bg-gray-900   h-full overflow-auto scrollbar-hide max-w-2xl sm:w-screen
+            <div id="modal-container" style="display:block;" class="z-50 top-0 bottom-0 right-0 left-0  w-screen h-full bg-black bg-opacity-80 flex items-center justify-center  ">
+              <div class="bg-gray-900 mb-28  h-full overflow-auto scrollbar-hide max-w-2xl sm:w-screen
 ">
                 <button onclick="toggleDetails()" class="absolute top-4 right-4 text-gray-400 hover:text-white">
                   <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">

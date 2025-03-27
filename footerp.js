973e3af4b0fe614f9f8a1d0cde2ca466cc77bac9
Oteng Emmanuel <{ID}+{username}@users.noreply.github.com>
2025-03-27 04:31:13 +0000

    ddddddd

--- footerp.js
+++ footerp.js
@@ -20,8 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
                         <li><a href="blog.html" class="flex items-center text-gray-400 hover:text-white"><span class="mr-2">📌</span> Blog posts</a></li>
                         <li><a href="courses.html" class="flex items-center text-gray-400 hover:text-white"><span class="mr-2">📚</span> Courses</a></li>
                         <li><a href="store.html" class="flex items-center text-gray-400 hover:text-white"><span class="mr-2">🛒</span> Shop</a></li>
-                        <li><a href="aboutus.html" class="flex items-center text-gray-400 hover:text-white"><span class="mr-2">📷</span> Gallery</a></li>
-                        <li><a href="aboutus.html" class="flex items-center text-gray-400 hover:text-white"><span class="mr-2">🎥</span> Free Videos</a></li>
+                        <li><a href="gallery.html" class="flex items-center text-gray-400 hover:text-white"><span class="mr-2">📷</span> Gallery</a></li>
+                        <li><a href="freevideos.html" class="flex items-center text-gray-400 hover:text-white"><span class="mr-2">🎥</span> Free Videos</a></li>
                     </ul>
                 </div>
                 
